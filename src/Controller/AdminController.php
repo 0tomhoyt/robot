955b9399ab84772d1739d9395950ec239093abe7
tomhoyt <houyuteng0501@163.com>
2023-09-21 19:43:54 +0100

    test1

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        //$this->denyAccessUnlessGranted("IS_AUTHENTICATED_FULLY");
+       /* return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',*/
+        //$user = $this->getUser();
+        //return match ($user->isVerified()) {
+            //true => $this->render("admin/index.html.twig"),
+            //false =>$this->redirectToRoute( 'ResendVerify' ),
+        return $this->render("admin/index.html.twig");
+        //};
+    }
+}
